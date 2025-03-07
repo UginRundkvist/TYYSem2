7421d872bb566ef9e9eaa713f5f36ce6f025cb4d
UginRundkvist <103577897+UginRundkvist@users.noreply.github.com>
2025-03-07 12:20:45 +0300

    task 9

---
+++ Tasks/Задание8/Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, n;
+            Console.Write("Введите переменную K: ");
+            k = int.Parse(Console.ReadLine());
+            Console.Write("Введите переменную N: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= k; i++) {
+                for (int j = 0; j <= n; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
